bc1c9a4c001d7796a7c0d87939b105d7b9296a26
o <o@immerda.ch>
2014-09-12 17:48:10 +0200

    moar data

--- data/experiments/plot.rb
+++ data/experiments/plot.rb
@@ -6,13 +6,30 @@ RUN = ARGV[0]
 res = YAML::load_file("./results-#{RUN}.yaml")
 
 f_run = File.open("runtime-#{RUN}.csv", 'w')
+f_gc  = File.open("gc_time-#{RUN}.csv", 'w')
+f_cycles  = File.open("gc_cycles-#{RUN}.csv", 'w')
 
 res.each do |data|
   new_runtime      = data[:new_runtime].collect {|r| r.to_f}
   baseline_runtime = data[:baseline_runtime].collect {|r| r.to_f}
+
+  new_gc = data[:new_percent_gc_time].to_f * new_runtime.median
+  baseline_gc = data[:baseline_percent_gc_time].to_f * baseline_runtime.median
+
+  new_cycles = data[:new_gc_cycles].to_f
+  baseline_cycles = data[:baseline_gc_cycles].to_f
+
   f_run.write("#{data[:benchmark]}, #{new_runtime.median}, #{new_runtime.variance}, #{baseline_runtime.median}, #{baseline_runtime.variance}\n")
+  f_gc.write("#{data[:benchmark]}, #{new_gc}, #{baseline_gc}\n")
+  f_cycles.write("#{data[:benchmark]}, #{new_cycles}, #{baseline_cycles}\n")
 end
 
 f_run.close
+f_gc.close
+f_cycles.close
 
-`gnuplot -e 'set term png;set output "runtime-#{RUN}.png"; set datafile separator ",";set xtics rotate by -45;set style fill solid 1.00 border 0;set style histogram errorbars;set style data histogram;plot "runtime.csv" using 2:3:xtic(1) title "new gc", "runtime-#{RUN}.csv" using 4:5:xtic(1) title "baseline"'`
+`gnuplot -e 'set term png;set output "runtime-#{RUN}.png"; set datafile separator ",";set xtics rotate by -45;set style fill solid 1.00 border 0;set style histogram errorbars;set style data histogram;plot "runtime-#{RUN}.csv" using 2:3:xtic(1) title "new gc", "runtime-#{RUN}.csv" using 4:5:xtic(1) title "baseline"'`
+
+`gnuplot -e 'set term png;set output "gc_time-#{RUN}.png"; set datafile separator ",";set xtics rotate by -45;set style fill solid 1.00 border 0;set style histogram;set style data histogram;plot "gc_time-#{RUN}.csv" using 2:xtic(1) title "new gc", "gc_time-#{RUN}.csv" using 3:xtic(1) title "baseline"'`
+
+`gnuplot -e 'set logscale y;set term png;set output "gc_cycles-#{RUN}.png"; set datafile separator ",";set xtics rotate by -45;set style fill solid 1.00 border 0;set style histogram;set style data histogram;plot "gc_cycles-#{RUN}.csv" using 2:xtic(1) title "new gc", "gc_cycles-#{RUN}.csv" using 3:xtic(1) title "baseline"'`
--- report.md
+++ report.md
@@ -1,14 +1,18 @@
 # Src
 
-* Code from https://github.com/o-/Rexperiments
-* New gc: 1c25a79d07666f649b04d07a9854928b37d1872a (branch new_gc)
-* baseline: 8276bb2108dcf6093f508b21e7e1f73d6d4213f6 (svn 66436)
+The main repository is found at https://github.com/o-/Rexperiments
+
+The new GC implementation resides in the branch new_gc, all data is based on 1c25a79d07666f649b04d07a9854928b37d1872a.
+
+The baseline is svn commit 66436 (8276bb2108dcf6093f508b21e7e1f73d6d4213f6).
 
 # Recording
 
-* https://github.com:o-/benchR (run-experiments, run-experiment.rb)
+For tools see: https://github.com:o-/benchR (run-experiments, run-experiment.rb)
 
 # Benchmarks
+
+The following two sources are used for benchmarks:
 
 * benchR:  https://github.com/allr/benchR.git
 * benchmarks: https://github.com/rbenchmark/benchmarks.git
@@ -17,6 +21,24 @@
 
 ## Runtime
 
-### Mmaped memory
+Overall Wall time
+
+### Mmap version
 
 ![](https://raw.githubusercontent.com/o-/repoRt/master/data/experiments/runtime-mmap.png)
+
+## GC Time
+
+Time spent in GC
+
+### Mmap version
+
+![](https://raw.githubusercontent.com/o-/repoRt/master/data/experiments/gc_time-mmap.png)
+
+## GC Cycles
+
+Number of GCs (minor & full)
+
+### Mmap version
+
+![](https://raw.githubusercontent.com/o-/repoRt/master/data/experiments/gc_cycles-mmap.png)
